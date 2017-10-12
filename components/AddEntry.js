aada3a81b23bc4e58cef937db2c37c9cbe0dde33
danilomartins <dglmartins@outlook.com>
2017-10-11 21:08:57 -0400

    Changes for Notifications

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text, Platform, StyleSheet } from 'react-native'
-import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue,
+  clearLocalNotification,
+  setLocalNotification
+} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
@@ -75,7 +81,8 @@ class AddEntry extends Component {
     this.toHome()
 
     submitEntry( { key, entry })
-    //Clear local notification
+    clearLocalNotification()
+      .then(setLocalNotification())
   }
 
   reset = () => {
